3b0e2f95eece111c2f1cb282a11d27eada0e824d
ThuyDuongDao <dtduongg@gmail.com>
2024-11-13 03:13:27 +0800

    fix unproportional image

--- script.js
+++ script.js
@@ -4,42 +4,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Define custom positions for the pulsing circle on each step, now using percentages
     const circlePositions = [
-        { top: "12%", left: "85%" },  // Position for Step 1
-        { top: "18.5%", left: "80%" },  // Position for Step 2
-        { top: "26.5%", left: "79%" },  // Position for Step 3
-        { top: "89%", left: "39%" },  // Position for Step 4
-        { top: "38%", left: "25%" },  // Position for Step 5
+        { top: "11%", left: "83%" },  // Position for Step 1
+        { top: "18%", left: "83%" },  // Position for Step 2
+        { top: "26%", left: "87%" },  // Position for Step 3
+        { top: "88%", left: "35%" },  // Position for Step 4
+        { top: "38%", left: "18%" },  // Position for Step 5
         { top: "27.3%", left: "37%" },  // Position for Step 6
-        { top: "47%", left: "23.5%" },  // Position for Step 7
+        { top: "47%", left: "16%" },  // Position for Step 7
         { top: "27.3%", left: "47%" },  // Position for Step 8
-        { top: "27.3%", left: "57%" },  // Position for Step 9
-        { top: "43%", left: "78%" },  // Position for Step 10
+        { top: "27.3%", left: "65%" },  // Position for Step 9
+        { top: "43%", left: "85%" },  // Position for Step 10
         { top: "75.5%", left: "43%" },  // Position for Step 11
         { top: "75.5%", left: "43%" },  // Position for Step 12
-        { top: "60%", left: "85.5%" },  // Position for Step 13
-        { top: "27.3%", left: "72%" },  // Position for Step 14
-        { top: "47%", left: "80.5%" },  // Position for Step 15
+        { top: "60%", left: "94.5%" },  // Position for Step 13
+        { top: "27.3%", left: "85%" },  // Position for Step 14
+        { top: "47%", left: "90%" },  // Position for Step 15
         { top: "49.5%", left: "39%" },  // Position for Step 16
-        { top: "43%", left: "73%" },  // Position for Step 17
+        { top: "43%", left: "85%" },  // Position for Step 17
         { top: "38.5%", left: "39%" },  // Position for Step 18
-        { top: "59.5%", left: "58.5%" },  // Position for Step 19
-        { top: "90%", left: "60.5%" },  // Position for Step 20
-        { top: "59.5%", left: "84.3%" },  // Position for Step 21
+        { top: "59.5%", left: "60.5%" },  // Position for Step 19
+        { top: "90%", left: "63%" },  // Position for Step 20
+        { top: "59.5%", left: "93%" },  // Position for Step 21
         { top: "18%", left: "38.5%" },  // Position for Step 22
-        { top: "32%", left: "37.6%" },  // Position for Step 23
+        { top: "32%", left: "34%" },  // Position for Step 23
         { top: "45%", left: "58%" },  // Position for Step 24
         { top: "45%", left: "58%" },  // Position for Step 25
         { top: "65.5%", left: "39%" },  // Position for Step 26
-        { top: "60%", left: "71.5%" },  // Position for Step 27
-        { top: "31.5%", left: "85%" },  // Position for Step 28
-        { top: "38.5%", left: "78%" },  // Position for Step 29
-        { top: "30%", left: "71.5%" },  // Position for Step 30
-        { top: "40%", left: "71.5%" },  // Position for Step 31
-        { top: "46%", left: "71.5%" },  // Position for Step 32
-        { top: "55%", left: "71.5%" },  // Position for Step 33
-        { top: "71%", left: "71.5%" },  // Position for Step 34
-        { top: "93.5%", left: "30%" },  // Position for Step 35
-        { top: "38%", left: "39%" },  // Position for Step 36
+        { top: "60%", left: "77%" },  // Position for Step 27
+        { top: "31.5%", left: "93%" },  // Position for Step 28
+        { top: "38.5%", left: "83%" },  // Position for Step 29
+        { top: "30%", left: "77%" },  // Position for Step 30
+        { top: "40%", left: "77%" },  // Position for Step 31
+        { top: "46%", left: "77%" },  // Position for Step 32
+        { top: "55%", left: "77%" },  // Position for Step 33
+        { top: "71%", left: "77%" },  // Position for Step 34
+        { top: "93.5%", left: "25%" },  // Position for Step 35
+        { top: "38%", left: "35%" },  // Position for Step 36
         // Add more positions for additional steps here
     ];
 
